db8e4ad4e05a3426981a1018d230a1cc17d8eb8b
Elliott Shugerman <eeshugerman@gmail.com>
2015-04-30 04:37:12 -0600

    Update prefixDictionary.cpp

--- prefixDictionary.cpp
+++ prefixDictionary.cpp
@@ -1,5 +1,7 @@
 #include "prefixDictionary.h"
 #include <iostream>
+#include <string>
+
 using namespace std;
 
 prefixDictionary::prefixDictionary()
@@ -11,76 +13,104 @@ prefixDictionary::~prefixDictionary()
 {
     //dtor
 }
-void::prefixDictionary::addWordPublic(string word)
+
+void prefixDictionary::addWord(string word)
 {
     node* z = new node;
-    addWord(root, z);
-}
-void prefixDictionary::addWord(node* root, node* z)
-{
+    z->word = word;
+    z->l = NULL;
+    z->r = NULL;
+
     node* x = root;
     node* p = NULL;
-    while(x != NULL)
-    {
-        p = x;
-        if(z->word.compare(x->word) < 0)
-        {
-            x = x->l;
-        }
-        else
-        {
-            x = x->r;
-        }
-    }
-    z->p = p;
-    if(p == NULL)
+
+    if (root == NULL)
     {
         root = z;
     }
-    else if(z->word.compare(x->word) < 0)
-    {
-        p->l = z;
-    }
     else
     {
-        p->r = z;
+        while(x != NULL)
+        {
+            p = x;
+            if(word.compare(x->word) < 0)
+            {
+                x = x->l;
+            }
+            else
+            {
+                x = x->r;
+            }
+        }
+        z->p = p;
+        if(word.compare(p->word) < 0)
+        {
+            p->l = z;
+        }
+        else
+        {
+            p->r = z;
+        }
     }
 }
 
-int prefixDictionary::isWordPublic(string word)
+bool prefixDictionary::isPrefixPublic(string word)
 {
-    return isWord(root, word);
+    return isPrefix(root,word);
 }
 
-int prefixDictionary::isWord(node* subTree, string word)
+bool prefixDictionary::isPrefix(node* subTree, string word)
 {
     if (subTree == NULL)
     {
-        return -1;
+        return false;
     }
 
-    else if (subTree->word == word)
+    if(word.length() < subTree->word.length())
     {
-        return 1;
-    }
-
-    else if(word.size() < subTree->word.size())
-    {
-        string nodePrefix = subTree->word.substr(0,word.size());
+        string nodePrefix = subTree->word.substr(0,word.length() + 1);
         if(nodePrefix == word)
         {
-            return 0;
+            return true;
         }
     }
 
-    else if(word.compare(subTree->word) < 0)
+    if(word.compare(subTree->word) < 0)
     {
-        isWord(subTree->l, word);
+        return isWord(subTree->l, word);
     }
 
     if(word.compare(subTree->word) > 0)
     {
-        isWord(subTree->r, word);
+        return isWord(subTree->r, word);
+    }
+}
+
+bool prefixDictionary::isWordPublic(string word)
+{
+    return isWord(root, word);
+}
+
+bool prefixDictionary::isWord(node* subTree, string word)
+{
+    if (subTree == NULL)
+    {
+        return false;
+    }
+
+    if (subTree->word == word)
+    {
+        return true;
+    }
+
+    if(word.compare(subTree->word) < 0)
+    {
+        return isWord(subTree->l, word);
+    }
+
+    if(word.compare(subTree->word) > 0)
+    {
+        return isWord(subTree->r, word);
     }
 
 }
